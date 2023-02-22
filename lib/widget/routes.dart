db09e986dd2fa1f69ab4a11a292bc6f7274084d8
IbrahimAlShawaf <a0594314120@rawafed.edu.ps>
2023-02-22 16:50:17 +0200

    first project getx

---
+++ lib/widget/routes.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class Routes {
+  static final routes = [
+    GetPage(name: '/', page: () => Home()),
+    // GetPage(name: '/profile', page: () => Profile()),
+    // GetPage(name: '/profile', page: () => Setting()),
+    // GetPage(name: '/profile', page: () => Profile()),
+    // GetPage(name: '/profile', page: () => Profile()),
+    // GetPage(name: '/profile', page: () => Profile()),
+    // GetPage(name: '/profile', page: () => Profile()),
+  ];
+}
